d6ea38ea2cabb1eff7b1a1a9fd25929d2175eb7d
Kenneth Choi <kennethichoi@gmail.com>
2024-01-01 21:47:34 -0800

    Update CORS

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, request
+from flask_cors import CORS
 import main
 
 app = Flask(__name__)
+
+cors = CORS(app, origins=[
+  "https://localhost:3000", 
+  "https://localhost", 
+  "https://www.kchoi.io", 
+  "https://kchoi-io-frontend.onrender.com/"
+  ])
 
 @app.post('/api/data')
 def create_query():
